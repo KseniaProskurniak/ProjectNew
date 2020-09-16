dd18ea7f9bb49a1baac775d4cc4facbd4e0227da
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2020-09-16 22:24:14 +0300

    Add

---
+++ src/Main/lesson1/task1/NewClass.java
@@ -0,0 +1,4 @@
+package Main.lesson1.task1;
+
+public class NewClass {
+}
---
+++ src/Main/lesson1/task1/Solution.java
@@ -0,0 +1,7 @@
+package Main.lesson1.task1;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
